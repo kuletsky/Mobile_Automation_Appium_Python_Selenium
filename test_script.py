c8b82a597906ceec2694c5257b326437981e23f7
Constantine Kuletsky <kuletsky@gmail.com>
2024-05-15 13:46:56 -0400

    added test_script

---
+++ test_script.py
@@ -0,0 +1,22 @@
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+from appium.options.android import UiAutomator2Options
+
+from time import sleep
+
+
+desired_capabilities = {
+    "platformName": "Android",
+    "automationName": "uiautomator2",
+    "platformVersion": "10",
+    "deviceName": "Android Emulator",
+    "appActivity": "org.wikipedia.main.MainActivity",
+    "appPackage": "org.wikipedia",
+    "app": "C:\Users\kuletsky\Mobile_Appium_Automation_Selenium_Python\mobile_app\wikipedia.xapk"
+}
+
+appium_server_url = 'http://127.0.0.1:4723'
+capabilities_options = UiAutomator2Options().load_capabilities(desired_capabilities)
+
+driver = webdriver.Remote(appium_server_url, options=capabilities_options)
+driver.implicitly_wait(5)
