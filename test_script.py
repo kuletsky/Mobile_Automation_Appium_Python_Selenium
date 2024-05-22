b18e68bfba2fd15a25643b2890dd0c8c74b85270
kuletsky <kuletsky@gmail.com>
2024-05-22 19:09:17 -0400

    finished wiki

--- test_script.py
+++ test_script.py
@@ -19,13 +19,22 @@ appium_server_url = 'http://127.0.0.1:4723'
 capabilities_options = UiAutomator2Options().load_capabilities(desired_capabilities)
 
 driver = webdriver.Remote(appium_server_url, options=capabilities_options)
-driver.implicitly_wait(50)
+driver.implicitly_wait(5)
 
 # click Skip onboarding
 driver.find_element(AppiumBy.ID, 'org.wikipedia:id/fragment_onboarding_skip_button').click()
 
 # click on Search icon
-# driver.find_element(AppiumBy.XPATH, '//android.widget.TextView[@text="Search Wikipedia"]').click()
+driver.find_element(AppiumBy.XPATH, '//android.widget.TextView[@text="Search Wikipedia"]').click()
 
 # search input
-# driver.find_element().send_keys('')
+driver.find_element(AppiumBy.ID, 'org.wikipedia:id/search_src_text').send_keys('Python (programming language)')
+
+# verify 
+actual_text = driver.find_element(AppiumBy.XPATH, "//*[@text='Python (programming language)' and @resource-id='org.wikipedia:id/page_list_item_title']").text
+# actual_text = driver.find_element(AppiumBy.ID, 'org.wikipedia:id/page_list_item_title11').text
+expected_text = 'Python (programming language)'
+
+assert expected_text == actual_text, f'Expected {expected_text} but got {actual_text}'
+
+driver.quit()
